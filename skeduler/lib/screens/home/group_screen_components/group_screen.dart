6320f54f5d8d3ce9f459852ad432c1370b4ba544
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2020-04-04 09:10:25 +0800

    Modified GroupScreen(): AppBar title text and icon color automatically changes to black if the shade is Shade.primaryColorLight

--- skeduler/lib/screens/home/group_screen_components/group_screen.dart
+++ skeduler/lib/screens/home/group_screen_components/group_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:skeduler/models/color_shade.dart';
 import 'package:skeduler/models/group.dart';
+import 'package:skeduler/models/my_app_themes.dart';
 import 'package:skeduler/screens/home/group_screen_components/group_screen_options.dart';
 import 'package:skeduler/screens/home/home_drawer.dart';
 import 'package:skeduler/services/database_service.dart';
 import 'package:skeduler/shared/components/loading.dart';
 import 'package:skeduler/shared/functions.dart';
 import 'package:skeduler/shared/ui_settings.dart';
+import 'package:theme_provider/theme_provider.dart';
 
 class GroupScreen extends StatefulWidget {
   final void Function({String groupName}) refresh;
@@ -39,17 +42,37 @@ class _GroupScreenState extends State<GroupScreen> {
             stream: dbService.getGroup(groupDocId.value),
             builder: (context, snapshot) {
               Group group = snapshot != null ? snapshot.data : null;
+              Color backgroundColor;
+              IconThemeData iconTheme;
+              TextTheme textTheme;
 
+              if (group != null) {
+                bool lightShade =
+                    group.colorShade.shade == Shade.primaryColorLight;
+
+                backgroundColor = getOriginThemeColorShade(group.colorShade);
+                iconTheme = lightShade
+                    ? ThemeProvider.themeOf(context)
+                        .data
+                        .iconTheme
+                        .copyWith(color: Colors.black)
+                    : getOriginThemeData(group.colorShade.themeId)
+                        .primaryIconTheme;
+                textTheme = lightShade
+                    ? ThemeProvider.themeOf(context)
+                        .data
+                        .textTheme
+                        .copyWith(title: TextStyle(color: Colors.black))
+                    : getOriginThemeData(group.colorShade.themeId)
+                        .primaryTextTheme;
+              }
               return snapshot == null || snapshot.data == null
                   ? Loading()
                   : Scaffold(
                       appBar: AppBar(
-                        backgroundColor:
-                            getOriginThemeColorShade(group.colorShade),
-                        iconTheme: getOriginThemeData(group.colorShade.themeId)
-                            .primaryIconTheme,
-                        textTheme: getOriginThemeData(group.colorShade.themeId)
-                            .primaryTextTheme,
+                        backgroundColor: backgroundColor,
+                        iconTheme: iconTheme,
+                        textTheme: textTheme,
                         title: group.name == null
                             ? Text(
                                 'Group',
