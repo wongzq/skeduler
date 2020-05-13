ad7859c196ba93cda5cff2d2c1672ae51e48d43f
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2020-05-13 13:08:55 +0800

    Cloud Functions: Modified index.ts: Added userNameUpdated firestore trigger Added groupMemberNicknameUpdated firestore trigger

--- skeduler_cloud_functions/functions/src/index.ts
+++ skeduler_cloud_functions/functions/src/index.ts
@@ -2,29 +2,93 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
-
 export const userNameUpdated = functions.firestore
   .document("/users/{userDocId}")
-  .onUpdate((change) => {
+  .onUpdate(async (change, context) => {
+    const before = change.before.data();
     const after = change.after.data();
 
-    const payload = {
-      data: {
-        temp: String(after!.temp),
-        conditions: after!.conditions,
-      },
-    };
+    if (before === null || after === null) {
+      return null;
+    } else if (before!.name !== after!.name) {
+      const userDocId = context.params.userDocId;
+      const groupsColRef = admin.firestore().collection("groups");
 
-    return admin
-      .messaging()
-      .sendToTopic("user name changed", payload)
-      .catch((error) => {
-        console.error()
+      const querySnapshot = await groupsColRef
+        .where("members", "array-contains", userDocId)
+        .get();
+
+      const promises: Array<Promise<any>> = [];
+
+      querySnapshot.forEach(async (groupDoc) => {
+        promises.push(
+          groupDoc.ref
+            .collection("members")
+            .doc(userDocId)
+            .update({ name: after!.name })
+        );
       });
+
+      return Promise.all(promises);
+    } else {
+      return null;
+    }
+  });
+
+export const groupMemberNicknameUpdated = functions.firestore
+  .document("/groups/{groupDocId}/members/{memberDocId}")
+  .onUpdate(async (change, context) => {
+    const before = change.before.data();
+    const after = change.after.data();
+    const groupDocId = context.params.groupDocId;
+
+    if (before === null || after === null) {
+      return null;
+    } else {
+      const oldNickname: String = before!.nickname;
+      const newNickname: String = after!.nickname;
+
+      if (oldNickname !== newNickname) {
+        const groupTimetablesColRef = admin
+          .firestore()
+          .collection("groups")
+          .doc(groupDocId)
+          .collection("timetables");
+
+        const querySnapshot = await groupTimetablesColRef.get();
+
+        const promises: Array<Promise<any>> = [];
+
+        querySnapshot.docs.forEach(async (timetable) => {
+          const gridDataList: Array<any> = timetable.data().gridDataList;
+
+          gridDataList.forEach((gridData) => {
+            if (gridData.member === oldNickname) {
+              const newGridData = Object.assign(Object(), gridData);
+              newGridData.member = newNickname;
+
+              promises.push(
+                groupTimetablesColRef.doc(timetable.id).update({
+                  gridDataList: admin.firestore.FieldValue.arrayRemove(
+                    gridData
+                  ),
+                })
+              );
+
+              promises.push(
+                groupTimetablesColRef.doc(timetable.id).update({
+                  gridDataList: admin.firestore.FieldValue.arrayUnion(
+                    newGridData
+                  ),
+                })
+              );
+            }
+          });
+        });
+
+        return Promise.all(promises);
+      } else {
+        return null;
+      }
+    }
   });
